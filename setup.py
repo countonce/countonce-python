3d2651d142c23cad676c314a4b5e567c0794c4c2
John Ginsberg <john.ginsberg@gmail.com>
2020-06-20 11:45:16 +0100

    Updating package name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as read_me:
     long_description = read_me.read()
 
 setup(
-    name='countonce_python',
+    name='countonce',
     version='0.1.0',
     author='CountOnce',
     url='https://github.com/countonce/countonce-python',
@@ -13,7 +13,7 @@ setup(
     install_requires=['requests'],
     python_requires='>=3.5',
     description='Wrapper for the countonce API',
-    long_description=long_description,
+    long_description='',
     long_description_content_type='text/markdown',
     keywords=['countonce', 'api']
 )
